e6eedb2101872584c42b8312e1b01815b0d119f5
Shep <alfredochsoliz@gmail.com>
2018-08-28 21:26:50 -0400

    added paginator to voluntario/index

--- src/app/components/usecases/voluntarios/voluntario-index/voluntario-index.component.ts
+++ src/app/components/usecases/voluntarios/voluntario-index/voluntario-index.component.ts
@@ -1,43 +1,102 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { VoluntarioModel } from "../../../../models/voluntario/voluntario.model";
 import { VoluntarioService } from "../../../../services/voluntario.service";
-import {animate, state, style, transition, trigger} from '@angular/animations';
-import { MatTableDataSource } from "../../../../../../node_modules/@angular/material";
-import { MatBottomSheetRef, MatBottomSheet } from '@angular/material';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from "@angular/animations";
+import {
+  MatTableDataSource,
+  MatPaginator
+} from "../../../../../../node_modules/@angular/material";
+import { MatBottomSheetRef, MatBottomSheet } from "@angular/material";
+import { UiService } from '../../../../services/ui.service';
+import {
+  Router,
+  RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from "../../../../../../node_modules/@angular/router";
 
 @Component({
   selector: "app-voluntario-index",
   templateUrl: "./voluntario-index.component.html",
   styleUrls: ["./voluntario-index.component.css"],
   animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
+    trigger("detailExpand", [
+      state(
+        "collapsed",
+        style({ height: "0px", minHeight: "0", display: "none" })
+      ),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      )
+    ])
+  ]
 })
 export class VoluntarioIndexComponent implements OnInit {
   voluntarios: VoluntarioModel[];
-
-  color:string='lightblue';
- // dataSource = ELEMENT_DATA;
+  loading: boolean = false;
+  color: string = "lightblue";
+  // dataSource = ELEMENT_DATA;
   // dataSource = this.voluntarios;
-   dataSource = new MatTableDataSource<VoluntarioModel>();
+  dataSource = new MatTableDataSource<VoluntarioModel>();
 
   // columnsToDisplay = ['name', 'weight', 'symbol', 'position'];
-  columnsToDisplay = ['nombre', 'apellidoPaterno', 'apellidoMaterno', 'celular'];
+  columnsToDisplay = [
+    "nombre",
+    "apellidoPaterno",
+    "apellidoMaterno",
+    "celular"
+  ];
   // expandedElement: PeriodicElement;
   expandedElement: VoluntarioModel;
+  @ViewChild(MatPaginator)
+  paginator: MatPaginator;
 
-
-  constructor(private voluntarioService: VoluntarioService,private bottomSheet: MatBottomSheet) {}
+  constructor(
+    private voluntarioService: VoluntarioService,
+    private bottomSheet: MatBottomSheet,
+    private _router: Router,private uiService:UiService
+  ) {
+    // _router.events.subscribe((event: RouterEvent) => {
+    //   this.navigationInterceptor(event);
+    // });
+  }
 
   ngOnInit() {
+      
+    this.uiService.useCaseStateChanged.next('Gestionar Usuario');
     this.voluntarioService.getVoluntarios().subscribe(voluntarios => {
       this.dataSource.data = voluntarios;
-      console.log(this.voluntarios);
+      console.log(voluntarios);
     });
+    this.dataSource.paginator = this.paginator;
+  }
+  ngAfterViewInit() {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    this._router.events
+            .subscribe((event) => {
+                if(event instanceof NavigationStart) {
+                    this.loading = true;
+                    console.log("Deberia cambiar");
+                    
+                }
+                else if (
+                    event instanceof NavigationEnd || 
+                    event instanceof NavigationCancel
+                    ) {
+                    this.loading = false;
+                }
+            });
   }
 
   applyFilter(filterValue: string) {
@@ -48,21 +107,39 @@ export class VoluntarioIndexComponent implements OnInit {
     this.bottomSheet.open(BottomSheetOverviewVoluntarioSheet);
   }
 
+  navigationInterceptor(event: RouterEvent): void {
+    console.log(event);
+
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+
+    // Set loading state to false in both of the below events to hide the spinner in case a request fails
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
 }
 
 @Component({
-  selector: 'bottom-sheet-overview-voluntario-sheet',
-  templateUrl: 'bottom-sheet-overview-voluntario-sheet.html',
+  selector: "bottom-sheet-overview-voluntario-sheet",
+  templateUrl: "bottom-sheet-overview-voluntario-sheet.html"
 })
 export class BottomSheetOverviewVoluntarioSheet {
-  constructor(private bottomSheetRef: MatBottomSheetRef<BottomSheetOverviewVoluntarioSheet>) {}
+  constructor(
+    private bottomSheetRef: MatBottomSheetRef<
+      BottomSheetOverviewVoluntarioSheet
+    >
+  ) {}
 
   openLink(event: MouseEvent): void {
     this.bottomSheetRef.dismiss();
     event.preventDefault();
   }
-
-  
 }
-
-
